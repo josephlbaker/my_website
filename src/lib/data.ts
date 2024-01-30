cb29da9e39d8e6f0376f21a49f30363440454a8e
Joseph Baker <jbaker@Josephs-Air.verizon.net>
2024-01-30 00:11:47 -0500

    Added header component

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -80,7 +80,7 @@ export const projectsData = [
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: wordanalyticsImg
   },
 ] as const;
 
