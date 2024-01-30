9f51e811a62582d9ac740b7fc128df42d0a73dc6
Joseph Baker <jbaker@Josephs-Air.verizon.net>
2024-01-30 15:46:42 -0500

    Created Summary component within intro section

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import React from 'react'
-import ProfileImage from './profile_image'
+import { motion } from 'framer-motion';
+import ProfileImage from './profile_image';
+import Summary from './summary';
 
 export default function Intro() {
     return (
-        <section>
+        <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
             <ProfileImage />
+            <Summary />
         </section>
     );
 }
