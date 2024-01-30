9b35158a05f81f7fa9f0bb918da629f2bdb19226
Joseph Baker <jbaker@Josephs-Air.verizon.net>
2024-01-30 18:46:57 -0500

    Created section divider component to place in between sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/src/components/intro/intro";
+import SectionDivider from "../components/section_divider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
+      <SectionDivider />
     </main>
   );
 }
