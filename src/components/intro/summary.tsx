e22ef460c5c235ed549c8fca61f793ed14811639
Joseph Baker <jbaker@Josephs-Air.verizon.net>
2024-01-31 11:48:14 -0500

    Changed p tag to h1 tag in the intro section

--- src/components/intro/summary.tsx
+++ src/components/intro/summary.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 
 export default function Summary() {
     return (
-        <motion.p
+        <motion.h1
             className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
@@ -15,6 +15,6 @@ export default function Summary() {
             <span className="font-bold">5 years</span> of experience. I enjoy
             building <span className="italic">sites & apps</span>. My focus is{" "}
             <span className="underline">React (Next.js)</span>.
-        </motion.p>
+        </motion.h1>
     );
 }
