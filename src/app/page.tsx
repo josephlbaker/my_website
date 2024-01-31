ac76e8365cca1161f91267a4b0b103df071919c7
Joseph Baker <jbaker@Josephs-Air.verizon.net>
2024-01-31 12:35:40 -0500

    Created about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Intro from "@/src/components/intro/intro";
 import SectionDivider from "../components/section_divider";
+import About from "../components/about";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
       <SectionDivider />
+      <About />
     </main>
   );
 }
