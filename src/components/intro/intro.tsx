8c8d22631c119b8214ec95362d5e51283d8418a7
Joseph Baker <jbaker@Josephs-Air.verizon.net>
2024-02-01 14:48:16 -0500

    Added custom scroll transitions to header so selected section changes on scroll and click

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import React from "react";
 
 import ProfileImage from "./profile_image";
 import Summary from "./summary";
 import SummaryLinks from "./summary_links";
+import { useSectionInView } from "@/src/lib/hooks";
 
 export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
+
   return (
     <section
+      ref={ref}
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
       id="home"
     >
--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,12 +1,16 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "../section_heading";
 import { projectsData } from "@/src/lib/data";
-
+import { useSectionInView } from "@/src/lib/hooks";
 import Project from "./project";
 
 export default function Projects() {
+  const { ref } = useSectionInView("Projects", 0.5);
+
   return (
-    <section className="scroll-mt-28" id="projects">
+    <section ref={ref} className="scroll-mt-28" id="projects">
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
