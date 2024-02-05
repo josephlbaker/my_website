3e96c105fda9df33cebe50f59507007c515db661
Joseph Baker <jbaker@Josephs-Air.verizon.net>
2024-02-05 15:01:24 -0500

    Commented out react stylings for email as it is currently bugged

--- src/actions/sendEmail.ts
+++ src/actions/sendEmail.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import React from "react";
 import { Resend } from "resend";
 import { validateString, getErrorMessage } from "../lib/utils";
+import ContactFormEmail from "@/src/email/contact_form_email";
+import ReactDOMServer from "react-dom/server";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -22,12 +25,18 @@ export const sendEmail = async (formData: FormData) => {
   }
 
   try {
+    // const emailContent = React.createElement(ContactFormEmail, {
+    //   senderEmail: senderEmail as string,
+    //   message: message as string,
+    // });
+
     await resend.emails.send({
       from: "Portfolio Contact Form <onboarding@resend.dev>",
       to: "joelucasbaker@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
       text: message as string,
+      //   react: emailContent,
     });
   } catch (error: unknown) {
     return {
