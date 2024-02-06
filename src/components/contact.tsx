fd128abddacf2343d055d072b69ed759c91ca740
Joseph Baker <jbaker@Josephs-Air.verizon.net>
2024-02-05 20:53:14 -0500

    Fixed scroll margin top so it doesn't get blocked when you click contact link

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -15,7 +15,7 @@ export default function Contact() {
     <motion.section
       ref={ref}
       id="contact"
-      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center scroll-mt-28"
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1 }}
